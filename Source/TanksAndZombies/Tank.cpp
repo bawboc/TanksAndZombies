082b349e17948f325e5f82a0441ad0abf7fedcfb
James Lawley <jhlawley@gmail.com>
2017-12-17 14:35:48 -0600

    Fixed minor issues with camera and tank movement so that tank moves correctly.

--- Source/TanksAndZombies/Tank.cpp
+++ Source/TanksAndZombies/Tank.cpp
@@ -27,11 +27,16 @@ ATank::ATank() {
 		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("TankBase"));
 	}
 
+	SpawnCollisionHandlingMethod = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
 	TankDirection = CreateDefaultSubobject<UArrowComponent>(TEXT("TankDirection"));
 	TankDirection->SetupAttachment(RootComponent);
 
 	TankSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("TankSprite"));
 	TankSprite->SetupAttachment(TankDirection);
+
+	ChildTurret = CreateDefaultSubobject<UChildActorComponent>(TEXT("Turret"));
+	ChildTurret->SetupAttachment(TankDirection);
 
 	USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->TargetArmLength = 500.0f;
@@ -40,20 +45,21 @@ ATank::ATank() {
 	SpringArm->bEnableCameraLag = true;
 	SpringArm->bEnableCameraRotationLag = false;
 	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->SetWorldRotation(FRotator(-90.0f, 0.0f, 0.0f));
-	SpringArm->SetupAttachment(TankDirection);
 
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComponent->bUsePawnControlRotation = false;
 	CameraComponent->ProjectionMode = ECameraProjectionMode::Orthographic;
 	CameraComponent->OrthoWidth = 1024.0f;
-	CameraComponent->AspectRatio = 3.0f / 4.0f;
-	CameraComponent->SetWorldRotation(FRotator(-90.0f, 0.0f, 0.0f));
+	CameraComponent->AspectRatio = 4.0f / 3.0f;
+	CameraComponent->SetWorldRotation(FRotator(-90.0f, -90.0f, 0.0f));
 	// Attach to end of spring arm
 	CameraComponent->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 
-	ChildTurret = CreateDefaultSubobject<UChildActorComponent>(TEXT("Turret"));
-	ChildTurret->SetupAttachment(TankDirection);
+	MoveSpeed = 100.0f;
+	YawSpeed = 180.0f;
+	
 }
 
 // Called when the game starts or when spawned
@@ -67,47 +73,49 @@ void ATank::Tick(float DeltaTime) {
 
 	TankInput.Sanitize();
 
-	//  Move the tank
+	// Respond to controls.
 	{
 		FVector DesiredMovementDirection = FVector(TankInput.MovementInput.X, TankInput.MovementInput.Y, 0.0f);
 		if (!DesiredMovementDirection.IsNearlyZero()) {
-			//  Rotate the tank.  Rotate the TankDirection component, not the RootComponent.
+			// Rotate the tank. This process deals with TankDirection directly, not the actor/RootComponent, because we don't want to affect the camera.
 			FRotator MovementAngle = DesiredMovementDirection.Rotation();
 			float DeltaYaw = UTankStatics::FindDeltaAngleDegrees(TankDirection->GetComponentRotation().Yaw, MovementAngle.Yaw);
 			bool bReverse = false;
 			if (DeltaYaw != 0.0f) {
-				float AdjustedYaw = DeltaYaw;
-
-				if (AdjustedYaw < -90.0f) {
-					AdjustedYaw += 180.0f;
+				float AdjustedDeltaYaw = DeltaYaw;
+				// If we're trying to go more than 90 degrees away from our current facing, reverse.
+				if (AdjustedDeltaYaw < -90.0f) {
 					bReverse = true;
-				} else if (AdjustedYaw > 90.0f) {
-					AdjustedYaw -= 180.0f;
+					AdjustedDeltaYaw += 180.0f;
+				} else if (AdjustedDeltaYaw > 90.0f) {
 					bReverse = true;
+					AdjustedDeltaYaw -= 180.0f;
 				}
 
-				//  Turn toward the desired angle.  Stop if we can get there in one frame
+				// Adjust toward our desired angle, and stop if we've reached it.
 				float MaxYawThisFrame = YawSpeed * DeltaTime;
-				if (MaxYawThisFrame >= FMath::Abs(AdjustedYaw)) {
+				if (MaxYawThisFrame >= FMath::Abs(AdjustedDeltaYaw)) {
 					if (bReverse) {
-						//  Move backward
+						// Move backward. Use a temp variable in case we need MovementAngle to be correct later.
 						FRotator FacingAngle = MovementAngle;
 						FacingAngle.Yaw = MovementAngle.Yaw + 180.0f;
 						TankDirection->SetWorldRotation(FacingAngle);
 					} else {
+						// Finish, moving forward. Facing and movement are the same angle.
 						TankDirection->SetWorldRotation(MovementAngle);
 					}
 				} else {
-					//  Can't reach our desired angle this frame, rotate part way.
-					TankDirection->AddLocalRotation(FRotator(0.0f,FMath::Sign(AdjustedYaw) * MaxYawThisFrame, 0.0f));
+					// Adjust as far as we can this frame, because we know we can't reach the goal yet.
+					TankDirection->AddLocalRotation(FRotator(0.0f, FMath::Sign(AdjustedDeltaYaw) * MaxYawThisFrame, 0.0f));
 				}
 			}
-			// Move the tank 
+
+			// Move the tank.
 			{
 				FVector MovementDirection = TankDirection->GetForwardVector() * (bReverse ? -1.0f : 1.0f);
 				FVector Pos = GetActorLocation();
-				Pos.X += MovementDirection.X * MoveSpeed * DeltaTime;
-				Pos.Y += MovementDirection.Y * MoveSpeed * DeltaTime;
+				Pos.X += MovementDirection.X * 100.0f * DeltaTime;
+				Pos.Y += MovementDirection.Y * 100.0f * DeltaTime;
 				SetActorLocation(Pos);
 			}
 		}
